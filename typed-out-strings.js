1196874e256edc9d99228754da44d6b42b7f68e3
Jess Vimm <jessvimm@gmail.com>
2022-11-15 13:13:11 -0600

    Added optimal solution for typed-out-strings

--- typed-out-strings.js
+++ typed-out-strings.js
@@ -40,5 +40,56 @@ var compare = function(firstStr, secondStr) {
     return true;
 };
 
-console.log(compare(str1, str2));
+var optimalComparison = function(firstStr, secondStr) {
+    let p1 = firstStr.length - 1, p2 = secondStr.length - 1;
+
+    // Until both strings were iterated
+    while(p1 >= 0 || p2 >=0) {
+        // Check if # appears in a string
+        if (firstStr[p1] === '#' || secondStr[p2] === '#') {
+            // If firstString has it, move p1 to the left
+            if (firstStr[p1] === '#') {
+                let catsCount = 2;
+                // Move until no cats
+                while(catsCount > 0) {
+                    p1--;
+                    catsCount--;
+
+                    // Found another '#'
+                    if (firstStr[p1] === '#') {
+                        catsCount += 2;
+                    } 
+                }
+            }
+            // If secondStr has '#', move p2 to the left
+            if (secondStr[p2] === '#') {
+                let catsCount = 2;
+                // Move until no cats
+                while(catsCount > 0) {
+                    p2--;
+                    catsCount--;
+
+                    // Found another '#'
+                    if (secondStr[p2] === '#') {
+                        catsCount += 2;
+                    } 
+                }
+            } 
+        } else {
+            // Compare chars
+            if (firstStr[p1] !== secondStr[p2]) {
+                // Not equal
+                return false;
+            } else {
+                // Move left
+                p1--;
+                p2--;
+            }
+        }
+    }
+    // They were equal
+    return true;
+};
+
+console.log(optimalComparison(str1, str2));
 
